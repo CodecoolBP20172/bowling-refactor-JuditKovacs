0870b5b3871cf932f96fc8f2dbb4a804bb516ebc
Jones <medonica1105@gmail.com>
2017-07-28 18:36:56 +0200

    get_value refactored

--- bowling.py
+++ bowling.py
@@ -7,7 +7,7 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
@@ -31,13 +31,11 @@ def score(game):
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x' or char == '/':
+    if char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
+    elif 1 <= int(char) <= 9:
+        return int(char)
     else:
         raise ValueError()
